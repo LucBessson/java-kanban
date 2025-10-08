b69fa20f2344c66633be6787b3559a8d52fcb338
Nikita <nikita220898@gmail.com>
2025-10-08 03:48:34 +0300

    Initial Commit

--- src/ru/yandex/javacourse/schedule/manager/InMemoryHistoryManager.java
+++ src/ru/yandex/javacourse/schedule/manager/InMemoryHistoryManager.java
@@ -1,72 +1,58 @@
 package ru.yandex.javacourse.schedule.manager;
 
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import ru.yandex.javacourse.schedule.tasks.Task;
 
 /**
- * In memory history manager.
+ * In-memory history manager.
  *
- * @author Vladimir Ivanov (ivanov.vladimir.l@gmail.com)
+ * @author Vladimir Ivanov
  */
 public class InMemoryHistoryManager implements HistoryManager {
 
-
 	private final MyLinkedList myLinkedList = new MyLinkedList();
-
-	private  final HashMap<Integer, Node> myHashMap = new HashMap<>();
-
-
+	private final HashMap<Integer, Node> myHashMap = new HashMap<>();
 
 	@Override
 	public List<Task> getHistory() {
 		return myLinkedList.getTasks();
-
 	}
 
 	@Override
 	public void remove(int id) {
-
 		if (myHashMap.containsKey(id)) {
 			removeNode(myHashMap.get(id));
 			myHashMap.remove(id);
 		}
-
 	}
 
 	public void clear() {
-
 		for (int id : myHashMap.keySet()) {
-
 			removeNode(myHashMap.get(id));
-
 		}
-
 		myHashMap.clear();
-
 	}
 
 	@Override
 	public void addTask(Task task) {
-
 		if (task == null) {
 			return;
 		}
 
-
-		//history.add(task);
 		int id = task.getId();
+
+		if (myHashMap.containsKey(id)) {
+			removeNode(myHashMap.get(id));
+		}
+
 		Node node = myLinkedList.linkLast(task);
-		if (myHashMap.containsKey(id)) removeNode(myHashMap.get(id));
 		myHashMap.put(id, node);
-
-
 	}
 
-
-	private  void removeNode(Node node) {
-
+	private void removeNode(Node node) {
 		if (node == null) {
 			return;
 		}
@@ -74,19 +60,18 @@ public class InMemoryHistoryManager implements HistoryManager {
 		Node prevNode = node.prev;
 		Node nextNode = node.next;
 
-
-
-
 		if (prevNode == null) {
 			myLinkedList.first = nextNode;
-		} else prevNode.next = nextNode;
+		} else {
+			prevNode.next = nextNode;
+		}
 
 		if (nextNode == null) {
 			myLinkedList.last = prevNode;
-		} else nextNode.prev = prevNode;
-
-
+		} else {
+			nextNode.prev = prevNode;
+		}
 	}
+
+
 }
-
-
